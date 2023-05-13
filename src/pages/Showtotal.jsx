1d4a541c1a6916e49db1c86a5ed7a850b3e1746a
bank006 <88577275+bank006@users.noreply.github.com>
2023-05-13 17:41:31 +0700

    delete update

--- src/pages/Showtotal.jsx
+++ src/pages/Showtotal.jsx
@@ -81,7 +81,7 @@ function Showtotal() {
                     
                 /> */}
                 <div>
-                    <h1>totalpricert fdgdfg</h1>
+                    <h1>totalpricert </h1>
                     {
                         datas.map((post,index)=>{
                             return <h1 key={index} >{post.total}</h1>
