1b4b259fcad4938c8b9fe69336efc8568efc9d9d
bank006 <88577275+bank006@users.noreply.github.com>
2023-05-16 18:35:08 +0700

    add

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -6,19 +6,24 @@ function Navbar() {
   return (
     <div className='container'>
       <div className='nav-bar'>
+        
         <div className='title' >
           <h1>menu</h1>
         </div>
+        
         <div className='menu'>
-          <div className='content'>
+          
+          <div for="home" className='content'>
             <Link className='onclick' to='/Home'>Home</Link>
           </div>
-          <div className='content'>
+          <div for="about" className='content'>
             <Link className='onclick' to='/'>About</Link>
           </div>
-          <div className='content'>
+          <div for="showtotal" className='content'>
             <Link className='onclick' to='/Showtotal' >Showtotal</Link>
           </div>
+          <div className='slider start-home'></div>
+          
         </div>
       </div>
       
